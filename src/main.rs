1e0b5da544b50810f9c2066e4944d8673e5c4892
jiro4989 <jiroron666@gmail.com>
2022-09-30 22:08:02 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = match line {
+            Ok(f) => f,
+            Err(e) => {
+                panic!("error {:?}", e)
+            }
+        };
+        let cols: Vec<&str> = line.split(":").collect();
+        let file_name = cols[0];
+        let line_num: usize = cols[1].parse().unwrap();
+        let file = File::open(file_name);
+        let file = match file {
+            Ok(f) => f,
+            Err(e) => {
+                panic!("error {:?}", e)
+            }
+        };
+        let reader = BufReader::new(file);
+        for (i, line) in reader.lines().enumerate() {
+            let line = match line {
+                Ok(f) => f,
+                Err(e) => {
+                    panic!("error {:?}", e)
+                }
+            };
+            if i == line_num {
+                print!("found! num = {}, str = {}", i, line);
+            }
+        }
+    }
 }
