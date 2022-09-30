2bc435522bbfb7f5a5b937007df4c1aeb5d3d029
jiro4989 <jiroron666@gmail.com>
2022-09-30 22:49:49 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::ptr::null;
 
 fn main() {
     let stdin = io::stdin();
@@ -33,4 +34,48 @@ fn main() {
             }
         }
     }
+}
+
+struct FileLine {
+    name: String,
+    num: u64,
+    line: String,
+}
+
+fn readline_surround_of_file(
+    name: &str,
+    line_num: u64,
+    before_line_count: u64,
+    after_line_count: u64,
+) -> Vec<FileLine> {
+    let file = File::open(name);
+    let file = match file {
+        Ok(f) => f,
+        Err(e) => {
+            panic!("error {:?}", e)
+        }
+    };
+
+    let cap = (before_line_count + 1 + after_line_count) as usize;
+    let mut vec: Vec<FileLine> = Vec::with_capacity(cap);
+    let num_range = (line_num - before_line_count)..(line_num + after_line_count);
+    let reader = BufReader::new(file);
+    for (i, line) in reader.lines().enumerate() {
+        let line = match line {
+            Ok(f) => f,
+            Err(e) => {
+                panic!("error {:?}", e)
+            }
+        };
+        let i = i as u64;
+        if num_range.contains(&i) {
+            let fl = FileLine {
+                name: name.to_string(),
+                num: i,
+                line: line,
+            };
+            vec.push(fl);
+        }
+    }
+    vec
 }
